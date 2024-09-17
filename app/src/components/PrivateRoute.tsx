fdf074b07945b2118e48ac6db066dc9d40101b8b
Samir Alam <samiralam@Wrong-Looper.local>
2024-09-18 00:47:39 +0545

    remove commented code from private route

--- app/src/components/PrivateRoute.tsx
+++ app/src/components/PrivateRoute.tsx
@@ -1,32 +1,3 @@
-// import React from "react";
-// import { Route, Navigate, Routes } from "react-router-dom";
-
-// const PrivateRoute = ({ component: Component, role, ...rest }: any) => {
-//   const isAuthenticated = !!localStorage.getItem("accessToken");
-//   const userRole = localStorage.getItem("userRole");
-
-//   return (
-//     <Routes>
-//       <Route
-//         {...rest}
-//         render={(props: any) =>
-//           isAuthenticated ? (
-//             role === userRole || role === "user" ? (
-//               <Component {...props} />
-//             ) : (
-//               <Navigate to="/unauthorized" />
-//             )
-//           ) : (
-//             <Navigate to="/login" />
-//           )
-//         }
-//       />
-//     </Routes>
-//   );
-// };
-
-// export default PrivateRoute;
-
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
