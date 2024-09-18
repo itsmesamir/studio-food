41625268f47c0d9119e36f02fa7170b684788851
bibekthapa007 <bibekthapa922@gmail.com>
2024-09-18 21:33:30 +0545

    Server staic file form app

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,10 @@
+import path from 'path';
+
+import express, { Application, Router, json, urlencoded } from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import compression from 'compression';
 import * as Sentry from '@sentry/node';
-import express, { Application, Router, json, urlencoded } from 'express';
 import cookieParser from 'cookie-parser';
 import 'express-async-errors';
 
@@ -16,37 +18,47 @@ import config from 'config';
 
 const log = logger.withNamespace('app');
 
+const appPath = path.join(__dirname, '../../app/build'); // Fixed build path reference
+
 class App {
   public app: Application;
 
   constructor(routes: Router) {
     this.app = express();
 
-    // TODO: Uncomment after setting up the sentry
-    // this.initializeUnhandelledErrorTracking();
     this.app.use(cors());
     this.app.use(helmet());
     this.app.use(compression());
 
+    this.app.use(express.static(path.join(appPath)));
+
+    // Catch all non-API routes and serve index.html
+    this.app.get('*', (req, res, next) => {
+      if (req.path.startsWith('/api')) {
+        return next();
+      }
+
+      return res.sendFile(path.join(appPath, 'index.html'));
+    });
+
     this.app.set('trust proxy', true);
     this.app.use(json());
     this.app.use(urlencoded({ extended: true }));
-
     this.app.use(cookieParser());
 
     this.app.use(initializeStore());
-
     this.app.use(requestLogger);
 
+    // Adding session to the store
     this.app.use(async (req, res, next) => {
       addToStore({ session: req.session });
-
       return next();
     });
 
     this.connectToDatabase();
 
     this.initializeAPIRoutes(routes);
+
     this.initializeErrorHandlers();
   }
 
@@ -58,13 +70,10 @@ class App {
 
   initializeAPIRoutes(routes: Router) {
     const baseURL = config.app.baseURL;
-
     this.app.use(baseURL, routes);
   }
 
   initializeUnhandelledErrorTracking() {
-    // Initialize Sentry
-    // https://docs.sentry.io/platforms/node/express/
     Sentry.init({
       dsn: config.sentry.dsn,
       environment: config.sentry.environment,
@@ -72,27 +81,23 @@ class App {
 
     this.app.use(Sentry.Handlers.requestHandler());
 
-    // Catch unhandled rejection
     process.on('unhandledRejection', err => {
       log.error(`Unhandled rejection ${err}`);
-
       try {
         Sentry.captureException(err);
       } catch (sentryCaptureError) {
-        log.error('Raven error', sentryCaptureError);
+        log.error('Sentry error', sentryCaptureError);
       } finally {
         process.exit(1);
       }
     });
 
-    // Catch uncaught exceptions
     process.on('uncaughtException', err => {
       log.error('Unhandled exception', err);
-
       try {
         Sentry.captureException(err);
       } catch (sentryCaptureError) {
-        log.error('Raven error', sentryCaptureError);
+        log.error('Sentry error', sentryCaptureError);
       } finally {
         process.exit(1);
       }
