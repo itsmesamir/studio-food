79565f6d54596c92deaef9336d8e179967f55182
Samir Alam <samiralam@Wrong-Looper.local>
2024-09-21 02:03:56 +0545

    add loading in tables

--- app/src/components/user/UsersTable.tsx
+++ app/src/components/user/UsersTable.tsx
@@ -4,33 +4,42 @@ import { User } from "interface/user";
 import { fetchUsers } from "services/users";
 import Table from "../table/Table";
 import { columns } from "./column";
-
-
+import TableTitle from "components/table/components/TableTitle";
 
 const UsersTable = () => {
   const [data, setData] = useState<User[]>([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const data = await fetchUsers(); 
-        setData(data); 
+        setLoading(true);
+        const data = await fetchUsers();
+        setData(data);
       } catch (error) {
-        console.error("Error fetching users:", error); 
+        console.error("Error fetching users:", error);
+      } finally {
+        setLoading(false);
       }
     };
 
     fetchData();
   }, []);
 
- 
- 
   return (
     <div>
+      <div className="flex justify-between">
+        <TableTitle
+          tableTitle="Users"
+          itemName={""}
+          start={data.length}
+          total={data.length}
+        />
+      </div>
       <Table<User>
-        loading={false}
+        loading={loading}
         columns={columns()}
-        data={data||[]}
+        data={data || []}
         getRowCanExpand={() => true}
         emptyMessage="No users found"
         parentClassName="px-4"
